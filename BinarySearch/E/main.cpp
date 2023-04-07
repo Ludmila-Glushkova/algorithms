3b3d9c57361596727e52ba4d13be97337cf44103
Ludmila-Glushkova <glushkovalm@yandex.ru>
2023-04-07 12:28:54 +0500

    E

---
+++ BinarySearch/E/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+typedef long double ld;
+
+int main()
+{
+    ld a, b, c, d;
+    std::cin >> a >> b >> c >> d;
+    ld l = -1, r = 1;
+    while ((pow(r, 3) * a + pow(r, 2) * b + r * c + d) * (pow(l, 3) * a + pow(l, 2) * b + l * c + d) >= 0)
+    {
+        r *= 2;
+        l *= 2;
+    }
+
+    while (r - l > 1e-7)
+    {
+        ld x;
+        x = (l + r) / 2;
+        if ((pow(x, 3) * a + pow(x, 2) * b + x * c + d) * (pow(r, 3) * a + pow(r, 2) * b + r * c + d) <= 0) l = x;
+        else r = x;
+    }
+    std::cout << std::setprecision(8) << l;
+}
