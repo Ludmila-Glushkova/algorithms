dd98b8e9017704cc368394d6d37eb5be7dd4f2f3
Ludmila-Glushkova <glushkovalm@yandex.ru>
2023-04-18 16:49:28 +0500

    F

---
+++ BinarySearch/F/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+int main()
+{
+    int n, k;
+    std::cin >> n >> k;
+    int* arr = new int [n] {};
+    for (int i = 0; i < n; ++i)
+        std::cin >> arr[i];
+    int l = 1, r = 1e9;
+    while (r - l > 1)
+    {
+        int m = (l + r) / 2;
+        int count = 0;
+        for (int i = 0; i < n; ++i)
+            count += arr[i] / m;
+        if (count >= k) l = m;
+        else r = m;
+    }
+    std::cout << l;
+}
